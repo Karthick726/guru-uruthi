188609292514c4bfbdaf5dea109cbc85eee5d5e4
Karthick726 <karthickc726@gmail.com>
2025-09-05 14:46:27 +0530

    shown

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,35 @@ export default defineConfig({
       // SEO
       "react-helmet-async",
 
-     
+      // Bootstrap + React Bootstrap deps
+      "bootstrap",
+      "countup.js",
+      "react-bootstrap",
+      "@restart/hooks",   // 👈 added
+      "dom-helpers",
+      "react-transition-group",
+
+      // Ant Design
+      "antd",
+      "@ant-design/cssinjs",
+
+      // MUI
+      "@mui/material",
+      "@mui/icons-material",
+      "@mui/lab",
+      "@mui/system",
+      "@mui/base",
+
+      // Animations & Carousels
+      "framer-motion",
+      "react-slick",
+      "slick-carousel",
+
+      // Utilities
+      "react-hot-toast",
+      "react-countup",
+      "react-icons",
+      "react-phone-input-2"
     ]
   }
 });
