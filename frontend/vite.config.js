444a14f22d99b851a2b0f06f2cafd1051fb14dc8
Karthick726 <karthickc726@gmail.com>
2025-09-05 14:41:21 +0530

    server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,49 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    ssr:true,
-    rollupOptions: {
-      input: {
-        client: './src/main.jsx',
-        server: './src/entry-server.jsx'
-      }
-    }
-  },
-   ssr: {
+  ssr: {
     noExternal: [
       // SEO
       "react-helmet-async",
 
-      // Bootstrap + React Bootstrap deps
-      "bootstrap", "countup.js",
-      "react-bootstrap",
-      "dom-helpers",
-      "react-transition-group",
-
-      // Ant Design
-      "antd",
-      "@ant-design/cssinjs",
-
-      // MUI
-      "@mui/material",
-      "@mui/icons-material",
-      "@mui/lab",
-      "@mui/system",
-      "@mui/base",
-
-      // Animations & Carousels
-      "framer-motion",
-      "react-slick",
-      "slick-carousel",
-
-      // Utilities
-      "react-hot-toast",
-      "react-countup",
-      "react-icons",
-      "react-phone-input-2"
-    ],
-  },
-})
+     
+    ]
+  }
+});
