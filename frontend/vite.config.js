b9985e0f94930730024055ec0fa9705c03612faa
Karthick726 <karthickc726@gmail.com>
2025-09-08 11:08:59 +0530

    push vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    ssr: true,
     rollupOptions: {
       input: {
-        main: "./index.html",
         client: "./src/main.jsx",
         server: "./src/entry-server.jsx",
       },
