704b8ef2881b81392d1445fb746ae498280e6e38
Karthick726 <karthickc726@gmail.com>
2025-09-08 11:13:21 +0530

    push to vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        main: "./index.html",
         client: "./src/main.jsx",
         server: "./src/entry-server.jsx",
       },
